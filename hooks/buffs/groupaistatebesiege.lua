16932406f352003d9ca99abbfcd4132dd9029fa4
offyerrocker <offyerrocker88@gmail.com>
2025-06-07 04:29:45 -0700

    changed winters buff detection
    captain winters buff detection changed from listening to hud feed, to listening to groupaistate buff setter

---
+++ hooks/buffs/groupaistatebesiege.lua
@@ -0,0 +1,10 @@
+-- this should be called regardless 
+Hooks:PostHook(GroupAIStateBesiege,"set_phalanx_damage_reduction_buff","kt_groupaistatebesiege_setcptwinters",function(self,damage_reduction)
+	local law1team = self:_get_law1_team()
+	local _damage_reduction = law1team.damage_reduction
+	if _damage_reduction and _damage_reduction > 0 then
+		managers.kinetictracker:AddBuff("winters_resistance",{value=_damage_reduction})
+	else
+		managers.kinetictracker:RemoveBuff("winters_resistance")
+	end
+end)
